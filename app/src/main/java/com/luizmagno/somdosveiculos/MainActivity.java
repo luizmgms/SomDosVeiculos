7ca5c82bf70ccb3d0d8e5a49496e410c2278b469
Luiz Magno <luiz.mg@hotmail.com>
2020-06-25 17:40:33 -0300

    Initial commit

---
+++ app/src/main/java/com/luizmagno/somdosveiculos/MainActivity.java
@@ -0,0 +1,41 @@
+package com.luizmagno.somdosveiculos;
+
+import android.os.Bundle;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbarInMainId);
+        setSupportActionBar(toolbar);
+        CollapsingToolbarLayout toolBarLayout = findViewById(R.id.toolbar_layout);
+        toolBarLayout.setTitle(getTitle());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_share) {
+            return true;
+        } else if (id == R.id.action_about) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
