4bbe41d7c0816803937775dcd38b0c985a621646
Luiz Magno <luiz.mg@hotmail.com>
2020-10-30 11:52:03 -0300

    Retirado Anúncios. Retiradas Tabs. Retirados Fragmentos. Retirado ViewPager.

--- app/src/main/java/com/luizmagno/somdosveiculos/MainActivity.java
+++ app/src/main/java/com/luizmagno/somdosveiculos/MainActivity.java
@@ -1,98 +1,54 @@
 package com.luizmagno.somdosveiculos;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
-
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdSize;
-import com.google.android.gms.ads.AdView;
-import com.google.android.material.appbar.CollapsingToolbarLayout;
-import com.google.android.material.tabs.TabLayout;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.viewpager.widget.ViewPager;
-
-import android.util.DisplayMetrics;
-import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.FrameLayout;
+import android.widget.ImageView;
 
-import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.initialization.InitializationStatus;
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.snackbar.Snackbar;
 
-    public static MediaPlayer mp;
-    private FrameLayout adContainerView;
+import java.util.ArrayList;
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if(mp != null) {
-            mp.stop();
-            mp.release();
-            mp = null;
-        }
-    }
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        //Inicializando Player
-        mp = new MediaPlayer();
-        mp.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mp) {
-                mp.start();
-            }
-        });
-    }
+    private MediaPlayer mp;
+    private ArrayList<Integer> vehicles;
+
+    private CoordinatorLayout coordinatorLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Anúncio
-        MobileAds.initialize(this, new OnInitializationCompleteListener() {
-            @Override
-            public void onInitializationComplete(InitializationStatus initializationStatus) {
-            }
-        });
-        adContainerView = findViewById(R.id.ad_view_container);
-        adContainerView.post(new Runnable() {
-            @Override
-            public void run() {
-                loadBanner();
-            }
-        });
+        //CoorinatorLayout
+        coordinatorLayout = findViewById(R.id.coordInMainId);
 
+        //Toolbar
         Toolbar toolbar = findViewById(R.id.toolbarInMainId);
         setSupportActionBar(toolbar);
 
-        CollapsingToolbarLayout toolBarLayout = findViewById(R.id.toolbar_layout);
-        toolBarLayout.setExpandedTitleColor(getResources().getColor(android.R.color.transparent));
-        toolBarLayout.setCollapsedTitleTextColor(
-            getResources().getColor(android.R.color.white));
-
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(
-            this, getSupportFragmentManager());
-
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(sectionsPagerAdapter);
-
-        TabLayout tabs = findViewById(R.id.tabs);
-        tabs.setupWithViewPager(viewPager);
-
-        setIconsOfTabs(tabs, sectionsPagerAdapter);
+        //CollapsingToolbar
+        CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.toolbar_layout);
+        collapsingToolbarLayout.setExpandedTitleColor(Color.TRANSPARENT);
+        collapsingToolbarLayout.setCollapsedTitleTextColor(Color.WHITE);
 
         //Inicializando Player
         mp = new MediaPlayer();
@@ -103,50 +59,41 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Lista de ID's de Veículos
+        vehicles = getIdsVehicles();
+
+        //SetClick
+        setClickArray(vehicles);
+
     }
 
-    private void setIconsOfTabs(TabLayout tabs, final SectionsPagerAdapter sectionsPagerAdapter) {
-
-        for (int i=0; i<sectionsPagerAdapter.getCount(); i++) {
-            if (i != 0) {
-                tabs.getTabAt(i).setIcon(sectionsPagerAdapter.getDrawable(i));
-                tabs.getTabAt(i).getIcon().setAlpha(80);
-            } else {
-                tabs.getTabAt(i).setIcon(sectionsPagerAdapter.getDrawable(i));
-            }
+    private void setClickArray(ArrayList<Integer> list) {
+        for (int id: list) {
+            findViewById(id).setOnClickListener(this);
         }
+    }
 
-        tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                int position = tab.getPosition();
-                if(position == 0) {
-                    tab.setIcon(R.drawable.ic_car);
-                } else if(position == 1) {
-                    tab.setIcon(R.drawable.ic_airplane);
-                } else {
-                    tab.setIcon(R.drawable.ic_boat);
-                }
+    private ArrayList<Integer> getIdsVehicles() {
+        ArrayList<Integer> list = new ArrayList<>();
 
-            }
+        list.add(R.id.aviaoJatoId);
+        list.add(R.id.carroId);
+        list.add(R.id.bicicletaId);
+        list.add(R.id.caminhaoId);
+        list.add(R.id.helicopId);
+        list.add(R.id.motoId);
+        list.add(R.id.ambuId);
+        list.add(R.id.aviaoMonoId);
+        list.add(R.id.bombId);
+        list.add(R.id.foguetId);
+        list.add(R.id.lixoId);
+        list.add(R.id.policiaId);
+        list.add(R.id.subId);
+        list.add(R.id.tratorId);
+        list.add(R.id.navioId);
+        list.add(R.id.tremId);
 
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-                int position = tab.getPosition();
-                if(position == 0) {
-                    tab.getIcon().setAlpha(80);
-                } else if(position == 1) {
-                    tab.getIcon().setAlpha(80);
-                } else {
-                    tab.getIcon().setAlpha(80);
-                }
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
+        return list;
     }
 
     @Override
@@ -217,32 +164,154 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private AdSize getAdSize() {
-        // Step 2 - Determine the screen width (less decorations) to use for the ad width.
-        Display display = getWindowManager().getDefaultDisplay();
-        DisplayMetrics outMetrics = new DisplayMetrics();
-        display.getMetrics(outMetrics);
-
-        float widthPixels = outMetrics.widthPixels;
-        float density = outMetrics.density;
-
-        int adWidth = (int) (widthPixels / density);
-
-        // Step 3 - Get adaptive ad size and return for setting on the ad view.
-        return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(this, adWidth);
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mp != null) {
+            mp.stop();
+            mp.release();
+            mp = null;
+        }
     }
 
-    private void loadBanner() {
-        AdView mAdView = new AdView(this);
-        mAdView.setAdUnitId(Developer.ID_OF_ANUN);
-        adContainerView.removeAllViews();
-        adContainerView.addView(mAdView);
-
-        AdSize adSize = getAdSize();
-        mAdView.setAdSize(adSize);
-
-        AdRequest adRequest = new AdRequest.Builder().build();
-        mAdView.loadAd(adRequest);
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //Inicializando Player
+        mp = new MediaPlayer();
+        mp.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mp.start();
+            }
+        });
     }
 
+    @Override
+    public void onClick(View v) {
+        playSound(v);
+    }
+
+    private void playSound(View v) {
+        try {
+
+            //Se estiver tocando, pare
+            if (mp.isPlaying()) {
+                mp.stop();
+            }
+
+            //Reset Player
+            mp.reset();
+
+            //Descritor de arquivo
+            AssetFileDescriptor afd;
+
+            //Id's id[0] = Nome, id[1] = Raw Som
+            int[] id = getIdNameSoundAnimal(v);
+
+            //AssetFileDescriptor recebe Raw
+            afd = getResources().openRawResourceFd(id[1]);
+
+            //Set Data Source e PrepareAsync
+            if (afd != null) {
+                mp.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+                mp.prepareAsync();
+            }
+
+            //Animação
+            startAnimation(v);
+
+            //Show SnackBar
+            Snackbar.make(coordinatorLayout, id[0], Snackbar.LENGTH_SHORT).show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Snackbar.make(coordinatorLayout, R.string.error, Snackbar.LENGTH_SHORT);
+        }
+    }
+
+    private void startAnimation(View v) {
+        ImageView imageView = (ImageView)v;
+        AnimationDrawable animationDrawable = (AnimationDrawable) imageView.getDrawable();
+        animationDrawable.stop();
+        animationDrawable.start();
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    private int[] getIdNameSoundAnimal (View v) {
+        int[] id = {0, 0};
+
+        switch (v.getId()) {
+            case R.id.aviaoJatoId:
+                id[0] = R.string.aviao_jato;
+                id[1] = R.raw.aviao_jato_1;
+                break;
+            case R.id.carroId:
+                id[0] = R.string.carro;
+                id[1] = R.raw.carro_1;
+                break;
+            case R.id.bicicletaId:
+                id[0] = R.string.bicicleta;
+                id[1] = R.raw.bicicleta_1;
+                break;
+            case R.id.caminhaoId:
+                id[0] = R.string.caminhao;
+                id[1] = R.raw.caminhao;
+                break;
+            case R.id.helicopId:
+                id[0] = R.string.helicop;
+                id[1] = R.raw.helicoptero_1;
+                break;
+            case R.id.motoId:
+                id[0] = R.string.moto;
+                id[1] = R.raw.moto;
+                break;
+            case R.id.ambuId:
+                id[0] = R.string.ambulancia;
+                id[1] = R.raw.ambu;
+                break;
+            case R.id.aviaoMonoId:
+                id[0] = R.string.aviao_mono;
+                id[1] = R.raw.aviao_mono_1;
+                break;
+            case R.id.bombId:
+                id[0] = R.string.bombeiro;
+                id[1] = R.raw.bombeiro;
+                break;
+            case R.id.foguetId:
+                id[0] = R.string.foguete;
+                id[1] = R.raw.foguete_1;
+                break;
+            case R.id.lixoId:
+                id[0] = R.string.lixo;
+                id[1] = R.raw.lixo_1;
+                break;
+            case R.id.policiaId:
+                id[0] = R.string.policia;
+                id[1] = R.raw.policia;
+                break;
+            case R.id.subId:
+                id[0] = R.string.sub;
+                id[1] = R.raw.sub_1;
+                break;
+            case R.id.tratorId:
+                id[0] = R.string.trator;
+                id[1] = R.raw.trator_1;
+                break;
+            case R.id.navioId:
+                id[0] = R.string.navio;
+                id[1] = R.raw.navio;
+                break;
+            case R.id.tremId:
+                id[0] = R.string.trem;
+                id[1] = R.raw.trem_1;
+                break;
+            default:
+                id[0] = 0;
+                id[1] = R.string.error;
+                break;
+        }
+
+        return id;
+    }
 }
